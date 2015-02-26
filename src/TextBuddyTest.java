88c9da74ba30a804697295f7ac2e18de62dfadfc
gary <garywjd91@gmail.com>
2015-02-26 23:12:17 +0800

    refined junit testing

--- src/TextBuddyTest.java
+++ src/TextBuddyTest.java
@@ -4,6 +4,7 @@ import org.junit.*;
 
 public class TextBuddyTest {	
 	static TextBuddy curr;
+	String newLine=System.lineSeparator();
 	private static String MSG_ADD="added to %s: \"%s\"\n";
 	private static String MSG_CLEAR="all content deleted from %s\n";
 	private static String MSG_DELETE="deleted from %s: \"%s\"\n";
@@ -24,7 +25,7 @@ public class TextBuddyTest {
 	public void testAddOperation() {	
 		assertEquals(String.format(MSG_ADD, fileName,"New Guinea"), curr.commandOperations("add New Guinea"));
 		assertEquals(String.format(MSG_ADD, fileName,"Ellesmere Island"), curr.commandOperations("add Ellesmere Island"));
-		System.out.println("out add "+curr.getListSize());
+		assertEquals(2,curr.getListSize());
 	}
 	
 	@Test
@@ -33,12 +34,9 @@ public class TextBuddyTest {
 		assertEquals(String.format(MSG_ADD, fileName,"King George Island"), curr.commandOperations("add King George Island"));
 		assertEquals(String.format(MSG_ADD, fileName,"Yos Sudarso"), curr.commandOperations("add Yos Sudarso"));
 		assertEquals(String.format(MSG_ADD, fileName,"Rangsang"), curr.commandOperations("add Rangsang"));
-		System.out.println("in delete "+curr.getListSize());
-		int randNum=1+(int)(Math.random()*curr.getListSize());
-		System.out.println("my rand num is "+randNum);
-		String toBeDeletedLine=curr.txtList.get(randNum-1);
-		System.out.println("line to be deleted "+toBeDeletedLine);
-		assertEquals(String.format(MSG_DELETE, fileName, toBeDeletedLine), curr.commandOperations("delete "+ randNum));
+		String expectedDeletedLine="Yos Sudarso";
+		assertEquals(String.format(MSG_DELETE, fileName, expectedDeletedLine), curr.commandOperations("delete 3"));
+		assertEquals(3,curr.getListSize());
 	}
 	
 	@Test
@@ -46,22 +44,20 @@ public class TextBuddyTest {
 		assertEquals(String.format(MSG_ADD, fileName,"Kodiak Island"), curr.commandOperations("add Kodiak Island"));
 		assertEquals(String.format(MSG_ADD, fileName,"King George Island"), curr.commandOperations("add King George Island"));
 		assertEquals(String.format(MSG_ADD, fileName,"Yos Sudarso"), curr.commandOperations("add Yos Sudarso"));
-		System.out.println("in search "+curr.getListSize());
-		
+		String expectedLines="1. Kodiak Island"+newLine+"2. King George Island"+newLine;
+		assertEquals(expectedLines, curr.commandOperations("search Island"));
 		
 	}
 	@Test
 	public void testSortAndDisplayOperation(){
-		String newLine=System.lineSeparator();
-		String sortedString="1. King George Island"+newLine+"2. Kodiak Island"+newLine+"3. Rangsang"+newLine+"4. Yos Sudarso"+newLine;
+		String expectedsortedLines="1. King George Island"+newLine+"2. Kodiak Island"+newLine+"3. Rangsang"+newLine+"4. Yos Sudarso"+newLine;
 		assertEquals(String.format(MSG_ADD, fileName,"Kodiak Island"), curr.commandOperations("add Kodiak Island"));
 		assertEquals(String.format(MSG_ADD, fileName,"King George Island"), curr.commandOperations("add King George Island"));
 		assertEquals(String.format(MSG_ADD, fileName,"Yos Sudarso"), curr.commandOperations("add Yos Sudarso"));
 		assertEquals(String.format(MSG_ADD, fileName,"Rangsang"), curr.commandOperations("add Rangsang"));
-		System.out.println("in sort "+curr.getListSize());
 		curr.commandOperations("sort");
-		String temp=curr.commandOperations("display");
-		assertEquals(sortedString, temp);
+		String actualLines=curr.commandOperations("display");
+		assertEquals(expectedsortedLines, actualLines);
 	}
 	@Test
 	public void testClearOperation(){
@@ -69,9 +65,8 @@ public class TextBuddyTest {
 		assertEquals(String.format(MSG_ADD, fileName,"King George Island"), curr.commandOperations("add King George Island"));
 		assertEquals(String.format(MSG_ADD, fileName,"Yos Sudarso"), curr.commandOperations("add Yos Sudarso"));
 		assertEquals(String.format(MSG_ADD, fileName,"Rangsang"), curr.commandOperations("add Rangsang"));
-		System.out.println("in clear "+curr.getListSize());
+		assertEquals(4, curr.getListSize());
 		assertEquals(String.format(MSG_CLEAR, fileName), curr.commandOperations("clear"));
 		assertEquals(0, curr.getListSize());
-		System.out.println("out clear "+curr.getListSize());
 	}
 }
